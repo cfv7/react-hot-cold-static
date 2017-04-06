920a5df9e0ca20b95e7159787d26d4e5b821ed9d
Alvin Law <alvinlaw@gmail.com>
2017-04-06 14:57:40 -0700

    working on the game - ally commits

--- src/components/game.js
+++ src/components/game.js
@@ -10,20 +10,34 @@ export default class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentGuess: 3,
-      guessList:[33, 10, 15],
-      count: 4,
-      feedback: 'YUP!'
+      currentGuess: 0,
+      guessList:[],
+      count: 0,
+      feedback: 'YUP!',
+      answer: 0
     }
   }
   // this.setState(e => e.target.value)
   // want to push the currentGuess to the guessList
   // function should be called commitGuess
-  commitGuess(guessInput){
-    console.log('commitGuess ran.');
-    console.log(guessInput);
-    this.setState({...this.state.guessList, guessInput});
+  newGame(){
+    this.setState({answer: Math.floor(Math.random() * 100)+1});
   }
+  commitGuess(guessInput){
+    let newcount= this.state.count+1;
+    this.setState({currentGuess:  guessInput});
+    console.log(this.state.guessList , guessInput);
+    if (this.state.guessList.includes(guessInput) > 0) {
+      alert('You already made this guess!');
+    } else if (guessInput>100 || guessInput<0 || isNaN(guessInput) ){
+      alert('Guess a number between 1-100');
+    } 
+    else {
+       this.setState({ guessList: [...this.state.guessList,guessInput]}); 
+       this.setState({count: newcount});
+    }
+  }
+
   render() { 
   return (
     <div>
